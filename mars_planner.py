91f5860f7715c3b8bf7fcc6a34aadbdf886043c0
mikesmith-23 <129104591+mikesmith-23@users.noreply.github.com>
2024-09-17 23:46:37 -0700

    Update mars_planner.py
    eq and goals functions done

--- mars_planner.py
+++ mars_planner.py
@@ -16,22 +16,30 @@
 from copy import deepcopy
 from search_algorithms import breadth_first_search
 
-class RoverState :
-    def __init__(self, loc="station", sample_extracted=False, holding_sample=False, charged=False):
+
+class RoverState:
+
+    def __init__(self,
+                 loc="station",
+                 sample_extracted=False,
+                 holding_sample=False,
+                 charged=False):
         self.loc = loc
-        self.sample_extracted=sample_extracted
+        self.sample_extracted = sample_extracted
         self.holding_sample = holding_sample
-        self.charged=charged
+        self.charged = charged
         self.prev = None
 
-    ## you do this.
-    def __eq__(self, other):
-       pass
-
+        ## you do this.
+        def __eq__(self, other):
+            return (self.loc == other.loc
+                    and self.sample_extracted == other.sample_extracted
+                    and self.holding_sample == other.holding_sample
+                    and self.charged == other.charged)
 
     def __repr__(self):
         return (f"Location: {self.loc}\n" +
-                f"Sample Extracted?: {self.sample_extracted}\n"+
+                f"Sample Extracted?: {self.sample_extracted}\n" +
                 f"Holding Sample?: {self.holding_sample}\n" +
                 f"Charged? {self.charged}")
 
@@ -49,42 +57,51 @@ class RoverState :
         succ = [item for item in succ if not item[0] == self]
         return succ
 
+
 ## our actions will be functions that return a new state.
 
-def move_to_sample(state) :
+
+def move_to_sample(state):
     r2 = deepcopy(state)
     r2.loc = "sample"
-    r2.prev=state
+    r2.prev = state
     return r2
-def move_to_station(state) :
+
+
+def move_to_station(state):
     r2 = deepcopy(state)
     r2.loc = "station"
     r2.prev = state
     return r2
 
-def move_to_battery(state) :
+
+def move_to_battery(state):
     r2 = deepcopy(state)
     r2.loc = "battery"
     r2.prev = state
     return r2
+
+
 # add tool functions here
 
 
-def pick_up_sample(state) :
+def pick_up_sample(state):
     r2 = deepcopy(state)
     if state.sample_extracted and state.loc == "sample":
         r2.holding_sample = True
     r2.prev = state
     return r2
 
-def drop_sample(state) :
+
+def drop_sample(state):
     r2 = deepcopy(state)
     if state.sample_extracted and state.loc == "station":
         r2.holding_sample = False
     r2.prev = state
     return r2
 
-def charge(state) :
+
+def charge(state):
     r2 = deepcopy(state)
     if state.sample_extracted and state.loc == "sample":
         r2.charged = True
@@ -92,21 +109,32 @@ def charge(state) :
     return r2
 
 
-action_list = [charge, drop_sample, pick_up_sample,
-               move_to_sample, move_to_battery, move_to_station]
+action_list = [
+    charge, drop_sample, pick_up_sample, move_to_sample, move_to_battery,
+    move_to_station
+]
 
-def battery_goal(state) :
-    return state.loc == "battery"
+
 ## add your goals here.
+def battery_goal(state):
+    return state.loc == "battery"
 
-def mission_complete(state) :
-    pass
+
+def charge_goal(state):
+    return state.charged
 
 
-if __name__=="__main__" :
+def sample_goal(state):
+    return state.sample_extracted and not state.holding_sample
+
+
+# True if we are at the battery, charged, and the sample is at the station.
+# False otherwise.
+def mission_complete(state):
+    return battery_goal(state) and charge_goal(state) and sample_goal(state)
+
+
+if __name__ == "__main__":
     s = RoverState()
     result = breadth_first_search(s, action_list, mission_complete)
     print(result)
-
-
-
