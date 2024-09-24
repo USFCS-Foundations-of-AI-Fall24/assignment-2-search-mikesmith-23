3143827c933e3dadf2a6b007ecf93298d8e6dd1e
mikesmith-23 <129104591+mikesmith-23@users.noreply.github.com>
2024-09-23 19:28:22 -0700

    Update mars_planner.py
    update tool logic

--- mars_planner.py
+++ mars_planner.py
@@ -14,7 +14,7 @@
 ## Charged can be True or False
 
 from copy import deepcopy
-from search_algorithms import breadth_first_search
+from search_algorithms import breadth_first_search, depth_first_search, depth_limited_search
 
 
 class RoverState:
@@ -23,115 +23,58 @@ class RoverState:
                  loc="station",
                  sample_extracted=False,
                  holding_sample=False,
-                 charged=False):
+                 charged=False,
+                 holding_tool=False):
         self.loc = loc
         self.sample_extracted = sample_extracted
         self.holding_sample = holding_sample
         self.charged = charged
+        self.holding_tool = holding_tool
         self.prev = None
 
-        ## you do this.
-        def __eq__(self, other):
-            return (self.loc == other.loc
-                    and self.sample_extracted == other.sample_extracted
-                    and self.holding_sample == other.holding_sample
-                    and self.charged == other.charged)
+    def __eq__(self, other):
+        return (self.loc == other.loc
+                and self.sample_extracted == other.sample_extracted
+                and self.holding_sample == other.holding_sample
+                and self.charged == other.charged
+                and self.holding_tool == other.holding_tool)
 
     def __repr__(self):
         return (f"Location: {self.loc}\n" +
                 f"Sample Extracted?: {self.sample_extracted}\n" +
                 f"Holding Sample?: {self.holding_sample}\n" +
-                f"Charged? {self.charged}")
+                f"Charged? {self.charged}\n" +
+                f"Holding Tool?: {self.holding_tool}")
 
     def __hash__(self):
-        return self.__repr__().__hash__()
+        return hash(
+            (self.loc, self.sample_extracted, self.holding_sample,
+             self.charged, self.holding_tool))
 
     def successors(self, list_of_actions):
-
-        ## apply each function in the list of actions to the current state to get
-        ## a new state.
-        ## add the name of the function also
         succ = [(item(self), item.__name__) for item in list_of_actions]
-        ## remove actions that have no effect
-
         succ = [item for item in succ if not item[0] == self]
         return succ
 
 
-## our actions will be functions that return a new state.
-
-
-def move_to_sample(state):
+def pick_up_tool(state):
     r2 = deepcopy(state)
-    r2.loc = "sample"
+    if state.loc == "station" and not state.holding_tool:
+        r2.holding_tool = True
     r2.prev = state
     return r2
 
 
-def move_to_station(state):
+def drop_tool(state):
     r2 = deepcopy(state)
-    r2.loc = "station"
+    if state.loc == "station" and state.holding_tool:
+        r2.holding_tool = False
     r2.prev = state
     return r2
 
 
-def move_to_battery(state):
-    r2 = deepcopy(state)
-    r2.loc = "battery"
-    r2.prev = state
-    return r2
-
-
-# add tool functions here
-
-
-def pick_up_sample(state):
-    r2 = deepcopy(state)
-    if state.sample_extracted and state.loc == "sample":
-        r2.holding_sample = True
-    r2.prev = state
-    return r2
-
-
-def drop_sample(state):
-    r2 = deepcopy(state)
-    if state.sample_extracted and state.loc == "station":
-        r2.holding_sample = False
-    r2.prev = state
-    return r2
-
-
-def charge(state):
-    r2 = deepcopy(state)
-    if state.sample_extracted and state.loc == "sample":
-        r2.charged = True
-    r2.prev = state
-    return r2
-
-
-action_list = [
-    charge, drop_sample, pick_up_sample, move_to_sample, move_to_battery,
-    move_to_station
-]
-
-
-## add your goals here.
-def battery_goal(state):
-    return state.loc == "battery"
-
-
-def charge_goal(state):
-    return state.charged
-
-
-def sample_goal(state):
-    return state.sample_extracted and not state.holding_sample
-
-
-# True if we are at the battery, charged, and the sample is at the station.
-# False otherwise.
-def mission_complete(state):
-    return battery_goal(state) and charge_goal(state) and sample_goal(state)
+action_list.append(pick_up_tool)
+action_list.append(drop_tool)
 
 
 if __name__ == "__main__":
